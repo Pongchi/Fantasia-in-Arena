d998b318adf72fde0fff0fa174fdd6c20747d59a
pongchi <jwj1219j@naver.com>
2024-01-02 14:48:26 +0000

    init canvas

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,30 +1,10 @@
 <script lang="ts">
-  export let name: string = '';
+  import Game from './components/Game.svelte';
 </script>
 
 <main>
-  <h1>Hello {name}!</h1>
+  <Game />
 </main>
 
 <style>
-  main {
-    text-align: center;
-    padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 2em;
-    font-weight: 900;
-    margin: 0;
-  }
-
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
-    }
-  }
 </style>
